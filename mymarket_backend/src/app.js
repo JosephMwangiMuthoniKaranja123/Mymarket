88f4346096cdfd6c6d43f82a6864756879c68ab8
Joseph Karanja <joserogerz123@gmail.com>
2026-03-08 13:11:11 +0300

    env changes

--- mymarket_backend/src/app.js
+++ mymarket_backend/src/app.js
@@ -11,7 +11,7 @@ import searchRoutes from "./routes/searchroutes.js";
 
 
 const app = express();
-const PORT=8080;
+const PORT=process.env.PORT || 8080;
 app.use(cors());
 app.use(express.json());
 app.use("/api/users",userRoutes);
